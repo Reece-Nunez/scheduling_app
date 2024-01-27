997826f1ba8a189f4a73b86694693162915ce40c
Reece Nunez <116838102+Reece-Nunez@users.noreply.github.com>
2024-01-27 02:33:27 +0000

    having issues with MIME type. Currently not being able to read CSS or JS files

--- scheduling_app/server/server.js
+++ scheduling_app/server/server.js
@@ -1,9 +1,20 @@
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.use(express.static(path.join(__dirname, '..client/dist'), {
+  setHeaders: (res, path) => {
+    if (path.endsWith('.js')) {
+      res.set('Content-Type', 'text/javascript');
+    } else if (path.endsWith('.css')) {
+      res.set('Content-Type', 'text/css');
+    }
+  }
+}));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 app.listen(port, () => {
